0e8e25965555650411a152a1d89c5710cc581a79
Jatin Singh Rathore <rathorejatin50103@gmail.com>
2026-03-08 11:15:26 +0530

    First commit - Treasure Island Python project

---
+++ Day 2/Number Manipulation/task.py
@@ -0,0 +1,7 @@
+score = 45
+
+print(f"Your score is {score}")
+
+a = int("5") / int(2.7)
+print(a)
+print(type(a))
---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the number you want to check: "))
+
+if num % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
